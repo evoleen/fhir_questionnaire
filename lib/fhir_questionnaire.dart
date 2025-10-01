00336d5d6b8cce2f4d577e487d82b2d9e8bad565
sh1l0n <jesusmanresaparres@protonmail.com>
2025-10-01 15:03:25 +0200

    trivial

--- lib/fhir_questionnaire.dart
+++ lib/fhir_questionnaire.dart
@@ -52,5 +52,3 @@ export 'package:fhir_questionnaire/src/presentation/localization/questionnaire_l
 export 'package:fhir_questionnaire/src/presentation/localization/questionnaire_es_localization.dart';
 export 'package:fhir_questionnaire/src/presentation/localization/questionnaire_en_localization.dart';
 export 'package:fhir_questionnaire/src/presentation/localization/questionnaire_base_localization.dart';
-export 'package:fhir_questionnaire/src/logic/utils/fhir_extension_utils.dart';
-export 'package:fhir_questionnaire/src/logic/utils/iterable_questionnaire_item_bundle.dart';
